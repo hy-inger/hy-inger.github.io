14827acc266c04a973b85067c1c6bbabcbdb04dc
huangying <huangying_01@youmi.net>
2016-06-24 17:43:19 +0800

    增加导航切换。

--- assets/js/about.js
+++ assets/js/about.js
@@ -5,7 +5,9 @@ window.onload = function(){
             num = section.length;
     var animation = function(next,prev,next_action,prev_action){
         var next_item = document.querySelector('.item'+next),
-                prev_item = document.querySelector('.item'+prev);
+                prev_item = document.querySelector('.item'+prev),
+                next_nav = document.querySelector('.step'+next),
+                prev_nav = document.querySelector('.step'+prev);
                 
                 module.addClass(next_item,next_action);
                 module.addClass(prev_item,prev_action);
@@ -16,6 +18,9 @@ window.onload = function(){
             module.removeClass(next_item,next_action);
             module.addClass(next_item,'active');  
 
+            module.addClass(next_nav,'active');
+            module.removeClass(prev_nav,'active');
+
             action = false;
             module.removeEvent(next_item,'webkitAnimationEnd',anim_end);  
         }
@@ -23,20 +28,14 @@ window.onload = function(){
         module.addEvent(next_item,'webkitAnimationEnd',anim_end);
              
     }
-    /*var animation_end = function(){
-        module.removeClass(prev_item,'active');
-        module.removeClass(prev_item,prev_action);
-
-        module.removeClass(next_item,next_action);
-        module.addClass(next_item,'active');
-    }*/
+    /* 鼠标滚轮事件切换页面 */
     module.addEvent(document,'mousewheel,DOMMouseScroll',function(e){
         if(action){
             return;
         }
         action = true;
         e.delta = (e.wheelDelta) ? e.wheelDelta / 120 : -(e.detail || 0) / 3;
-        if(e.delta<0){
+        if(e.delta<0){              // 滚轮向下,当前页面缩小向下隐藏,
             next = now+1;
             if(next>num){
                 action = false;
@@ -45,7 +44,7 @@ window.onload = function(){
             prev = now;
             now = next;
             animation(next,prev,'next-slide-up','prev-scale-down');
-        } else {
+        } else {                        // 滚轮向上,当前页面大小不变向下隐藏,
             if(now==1){
                 action = false;
                 return;
@@ -55,6 +54,32 @@ window.onload = function(){
             now = next;
             animation(next,prev,'next-scale-up','prev-slide-down');
         }
-        
     })
+    /* 点击导航切换页面,事件委托 */
+    var ul = document.getElementById('nav');
+    module.addEvent(ul,'click',function(e){
+        if(action){
+            setTimeout(function(){
+                action = false;
+            },300000);
+            return;
+        }
+        action = true;
+        var target = e.target?e.target:e.srcElement;
+        if(module.hasClass(target,'step')){
+            e.stopPropagation();
+            next = target.getAttribute('data');
+            prev = now;
+            if(now == next){
+                action = false;
+                return;
+            }
+            if(now < next){
+                animation(next,prev,'next-slide-up','prev-scale-down');
+            } else {
+                animation(next,prev,'next-scale-up','prev-slide-down');
+            }
+            now = next;
+        }
+    },true);
 }
